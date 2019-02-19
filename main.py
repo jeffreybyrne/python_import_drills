b9fbb06b11fae087f10130c4ce68871eaa23ab14
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-19 14:33:40 -0500

    added the import statements

---
+++ main.py
@@ -0,0 +1,11 @@
+#Your goal is to import and run functions one, two, three, four, five, and six. This should be done inside the Python shell
+#from the top-level directory (don't cd into the sub-directories).
+import ascii
+import scene
+from art.even_more_art import images
+
+print(one())
+print(two())
+print(three())
+print(four())
+print(five())
